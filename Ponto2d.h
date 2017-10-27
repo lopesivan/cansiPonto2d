710a80b6851a78a00d41f7d0b08e0038720b3465
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-10-27 01:07:20 -0200

    Atualiza o arquivo Ponto2d.h

--- Ponto2d.h
+++ Ponto2d.h
@@ -5,10 +5,11 @@
 #ifndef CANSIPONTO2D_PONTO2D_H
 #define CANSIPONTO2D_PONTO2D_H
 
-typedef struct Ponto2d_t* Ponto2d;
+typedef struct Ponto2d_t *Ponto2d;
 
-Ponto2d ponto_novo(float x, float y);
-void ponto_remove(Ponto2d ponto);
+Ponto2d ponto_novo (float x, float y);
+
+void ponto_remove (Ponto2d ponto);
 
 
 #endif //CANSIPONTO2D_PONTO2D_H
